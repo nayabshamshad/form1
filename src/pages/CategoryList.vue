65ea99dc9dc646657efa91f6d74ef17db74c9e2a
nayab shamshad <nayabshamshad90@gmaail.com>
2022-08-31 04:29:18 -0700

    'a'

--- src/pages/CategoryList.vue
+++ src/pages/CategoryList.vue
@@ -1,35 +1,74 @@
 <template>
   <div class="container">
-   
     <form class="form">
       <h2>Step 2</h2>
-      <label for="Region"><b>Region:</b></label>
-      <input type="text" v-model="regionInput" placeholder="Region" name="region" />
-      <label for="state"><b>State:</b></label>
-      <input type="text" v-model="stateInput" placeholder="State" name="State" />
-      <label for="telephone no."><b>Telephone No.</b></label>
-      <input type="text" v-model="emailInput" placeholder="+92......." name="phone number" />
-      <label for="year"><b>Year:</b></label>
-      <input type="text" v-model="yearInput" placeholder="Year" name="Year" />
-      <label for="year"><b>Year:</b></label>
-      <input type="text" v-model="yearInput" placeholder="Year" name="Year" />
-      <label for="year"><b>Year:</b></label>
-      <input type="text" v-model="yearInput" placeholder="Year" name="Year" />
-      <label for="tags"><b>Tags:</b></label>
-      <input type="text" v-model="tagsInput" placeholder="Tags" name="tags" />
-      <label for="club"><b>Club:</b></label>
-      <input type="text" v-model="clubInput" placeholder="Club" name="Club" />
-      <label for="list"><b>List:</b></label> 
-      <div class="input">
-      <input type="text" v-model="Input" placeholder="wasiq" name="wasiq" />
-      <button class="btn">+</button> </div> 
-      <label for="Category"><b>Category:</b></label>
-      <select>
+      <div>
+        <label for="club"><b>Club:</b></label>
+        <input type="text" v-model="clubInput" placeholder="Club" name="Club" />
+      </div>
+      <div>
+        <label for="date"><b>Date.</b></label>
+        <input type="date" id="date" name="date" />
+      </div>
+      <div>
+        <label for="tags"><b>Tags:</b></label>
+        <input type="text" v-model="tagsInput" placeholder="Tags" name="tags" />
+      </div>
+      <div class="radio">
+        <label for="status"><b>Status:</b></label> &nbsp; &nbsp;
+        <input type="radio" name="status" value="active" /> Active
+        <input type="radio" name="status" value="deactive" />Inactive
+      </div>
+      <div>
+        <label for="telephone no."><b>Telephone No.</b></label>
+        <input
+          type="text"
+          v-model="emailInput"
+          placeholder="+92......."
+          name="phone number"
+        />
+      </div>
+      <div>
+        <label for="state"><b>State:</b></label>
+        <input
+          type="text"
+          v-model="stateInput"
+          placeholder="State"
+          name="State"
+        />
+      </div>
+      <div>
+        <label for="Region"><b>Region:</b></label>
+        <input
+          type="text"
+          v-model="regionInput"
+          placeholder="Region"
+          name="region"
+        />
+      </div>
+      <div class="radio">
+        <label for="Gender"><b>Gender:</b></label> &nbsp;
+        <input type="radio" name="gender" value="male" /> Male
+        <input type="radio" name="gender" value="female" /> Female
+      </div>
+      <div>
+        <label for="Etnic"><b>Etnic:</b></label>
+        <select>
+          <option value="Etnic">Etnic</option>
+          <option value="a">a</option>
+          <option value="b">b</option>
+        </select>
+      </div>
+      <div>
+        <label for="Category"><b>Category:</b></label>
+        <select>
           <option value="Category">Category</option>
           <option value="a">a</option>
           <option value="b">b</option>
           <option value="c">c</option>
         </select>
+      </div>
+      <div>
         <label for="Size"><b>Size:</b></label>
         <select class="select">
           <option value="Size">Size</option>
@@ -40,28 +79,38 @@
           <option value="XXLarge">XXLarge</option>
           <option value="XXXLarge">XXXLarge</option>
         </select>
-        <label for="date"><b>Date.</b></label> <br>
-        <input type="date" id="date" name="date">
-       <br> 
-      <div class="radio">
-        <label for="Etnic"><b>Etnic:</b></label> &nbsp; &nbsp; &nbsp;
-        <input type="radio" name="gender" value="male"> A
-        <input type="radio" name="gender" value="female"> B &nbsp; &nbsp;
       </div>
-      <div class="radio"> <label for="Gender"><b>Gender:</b></label> &nbsp;
-        <input type="radio" name="gender" value="male"> Male
-        <input type="radio" name="gender" value="female"> Female
+      <div>
+        <label for="Instructor"><b>Instructor:</b></label>
+        <input
+          type="text"
+          v-model="yearInput"
+          placeholder="Instructor"
+          name="Instructor"
+        />
       </div>
-      <div class="radio">
-        <label for="status"><b>Status:</b></label> &nbsp; &nbsp;
-        <input type="radio" name="status" value="active"> Active
-        <input type="radio" name="status" value="deactive">Deactive
+      <div>
+        <label for="Ghid"><b>Ghid:</b></label>
+        <input type="text" v-model="yearInput" placeholder="Ghid" name="Ghid" />
+      </div>
+      <div>
+        <label for="Master Ghid"><b>Master Ghid:</b></label>
+        <input
+          type="text"
+          v-model="yearInput"
+          placeholder="Master Ghid"
+          name="Master Ghid"
+        />
+      </div>
+      <div class="input">
+        <label for="list"><b>List:</b></label>
+
+        <input type="text" v-model="Input" placeholder="wasiq" name="wasiq" />
+        <button class="btn">+</button>
       </div>
       <div class="btn1">
         <button type="button" class="signupbtn">Submit</button>
       </div>
-      
-    
     </form>
   </div>
 </template>
