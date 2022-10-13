bf4832176ddb51ae3d7968c5fb895b2abf0ec78a
nayab shamshad <nayabshamshad90@gmaail.com>
2022-10-13 04:23:26 -0700

    update

--- src/pages/rejectedView.vue
+++ src/pages/rejectedView.vue
@@ -2,13 +2,13 @@
   <div class="">
     <q-card style="height: 100vh;" class="q-py-lg q-px-lg">
       <q-card-section class="q-py-lg q-my-lg q-px-lg">
-        <div class="q-my-lg"><q-icon @click="logOut" class="cursor-pointer linkcolor" name="west" size="xl"></q-icon></div>
-        <h3 class="text-weight-bold text-center q-py-lg q-mt-lg">
+        <h2 class="text-center text-weight-bold reset-lec">LEC</h2>
+        <div class="q-my-lg reject-icon"><q-icon @click="logOut" class="cursor-pointer linkcolor" name="west" size="xl"></q-icon></div>
+        <h3 class="text-weight-bold text-center q-py-lg q-mt-lg reject-h3">
           Cerere respinsă
         </h3>
         <h6
-          class="q-mx-auto text-weight-light text-center"
-          style="max-width: 70%"
+          class="q-mx-auto  text-center reject-h6"
         >
           Cererea ta a fost respinsă, dacă crezi ca este o greșeală, poți
           retrimite o cerere administratorului.
@@ -16,12 +16,12 @@
         <div class="q-pt-lg q-mt-lg flex items-center justify-center" style="gap: 1rem;">
           <q-btn
           outline
-            class="linkcolor"
+            class="linkcolor reject-btn"
             flat
             :loading="isFetching"
             @click="logOut"
           >
-            Retrimite
+          ÎNAPOI
           </q-btn>
           <q-btn
             class="bg-linkcolor"
